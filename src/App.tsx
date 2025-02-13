dd2b920b6b9aaead4dd984d430dd0b7a2e5da633
DmitriyGulevich <kaskodim@gmail.com>
2025-02-13 20:38:06 +0300

    adds viewMode corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import {SingleCounter} from './pages/SingleCounter/SingleCounter';
 import {DoubleCounter} from './pages/DoubleCounter/DoubleCounter';
-import {AppStyles, ViewModeTitle} from './styles/AppStyles';
-import {Button} from './styles/Button';
-import {Wrapper} from './styles/Counter';
-
-type viewModeType = 'single' | 'double';
+import {AppStyles} from './styles/AppStyles';
+import {ViewSelector} from './components/ViewSelector';
+import {ViewModeType} from './types/types';
 
 function App() {
-
-    const [viewMode, setViewMode] = React.useState<viewModeType>('single');
-    const onClickViewModeHandler = () => {
-        viewMode === 'single' ? setViewMode('double') : setViewMode('single')
-    }
+    const [viewMode, setViewMode] = React.useState<ViewModeType>('single');
 
     return (
         <AppStyles>
             {viewMode === 'single' && <SingleCounter/>}
             {viewMode === 'double' && <DoubleCounter/>}
 
-            <Wrapper>
-                <ViewModeTitle>{viewMode}</ViewModeTitle>
-                <Button onClick={onClickViewModeHandler}>
-                    change it
-                </Button>
-            </Wrapper>
+            <ViewSelector viewMode={viewMode}
+                          setViewMode={setViewMode}/>
+
         </AppStyles>
     );
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,4 @@ export type ValuesType = {
 }
 export type FieldType = keyof ValuesType;
 export type StatusType = 'error' | 'setup' | 'ready';
+export type ViewModeType = 'single' | 'double';
